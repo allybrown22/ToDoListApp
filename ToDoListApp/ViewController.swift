fcfdd441571b7e224ee5e5741be64cc50fe27d4c
ally <aeb9226@gmail.com>
2021-06-14 15:52:35 -0400

    print statement complete

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Ally Brown on 6/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("done?")
+    }
+
+
+}
+
